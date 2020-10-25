f76653edffb12c0016eb57a03d6de66ebd7f7cab
Theodor Blom <theodor.blom1@gmail.com>
2020-10-25 23:29:47 +0100

    Added webpage files

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Trying out HTML/CSS/JS and MySQL!">
+        <meta name=viewport content="width=device-width, initial-scale=1.0">
+
+        <title>TestSite [WIP]</title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require "header.php";
+?>
+
+<main>
+    Test content!
+</main>
+
+<?php
+    require "footer.php";
+?>
