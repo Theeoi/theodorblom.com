124654aaca57b48debfd2ac600f80a539d1d9e9f
Theodor Blom <theodor.blom1@gmail.com>
2020-12-16 22:54:34 +0100

    Hostinger css-link not working. Fixing it now?

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 
         <title>Theodor Blom</title>
     
-        <link rel="stylesheet" href="resources/css/main.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/main.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- Imports Icons --> 
 
     </head>
