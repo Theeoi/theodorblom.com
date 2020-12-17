0494236fccb3783e43bda264841c579b6e93da9e
Theodor Blom <theodor.blom1@gmail.com>
2020-12-17 09:33:53 +0100

    Removed FB and added SoundCloud in header.

--- header.php
+++ header.php
@@ -15,12 +15,12 @@
         <header>
             <div class="header-content">
                 <div class="socials-btn" style="float:right">
-                    <a href="https://www.facebook.com/theodor.blom.5" class="fa fa-facebook"></a> 
                     <a href="https://www.instagram.com/theoalexanderb/" class="fa fa-instagram"></a> 
                     <a href="https://twitter.com/TheodorBlom" class="fa fa-twitter"></a> 
                     <a href="https://www.linkedin.com/in/theodor-blom-35498a133/" class="fa fa-linkedin"></a> 
                     <a href="https://www.reddit.com/user/Theeoii" class="fa fa-reddit"></a> 
                     <a href="https://github.com/Theeoi" class="fa fa-github"></a> 
+                    <a href="https://soundcloud.com/user-270867297" class="fa fa-soundcloud"></a> 
                 </div>
             </div> 
         </header>
