0afa663c30163ee80dc0a1e98ba026b76e56303b
Theodor Blom <theodor.blom1@gmail.com>
2022-01-03 11:02:29 +0100

    Started redesign.

--- footer.php
+++
@@ -1,7 +0,0 @@
-        <footer align="center">
-            <div class="footer-content">
-                <a href="mailto:me@theodorblom.com">me@theodorblom.com</a>
-            </div>
-        </footer>
-    </body>
-</html>
