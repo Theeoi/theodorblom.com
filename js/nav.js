8519ecfecbfd879a29b6895dc9b6f55695e28106
Theodor Blom <theodor.blom1@gmail.com>
2022-01-03 23:07:50 +0100

    Added sticky header and navbar.

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,6 @@
 
+/* Nav Toggle */
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
@@ -11,4 +13,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+
+/* Sticky Header */
+
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("sticky-header");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
 
