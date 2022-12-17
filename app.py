e3ffa02c509ffa34560acbfbd9136e492a00a6de
Theodor Blom <me@theodorblom.com>
2022-12-17 21:18:04 +0100

    feat(blog): added blog page

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def homepage():
     return render_template("index.html")
 
 
+@app.route("/blog/")
+def blog():
+    return render_template("blog.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
