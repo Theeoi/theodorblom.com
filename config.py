7620fdc1589afdbf588280ef46b13c91cec7a417
Theodor Blom <me@theodorblom.com>
2022-12-21 19:38:21 +0100

    feat(auth): created SQL database

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ Production variables are configured on the server.
 HOST = "0.0.0.0"
 DEBUG = True
 SECRET_KEY = "dev"
+SQL_DB_NAME = "database.db"
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{SQL_DB_NAME}"
--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 """App definitions of the website package."""
+from os import path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
@@ -8,6 +12,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
     app.config.from_pyfile('config.py', silent=True)
+    db.init_app(app)
 
     # import and register views here
     from .views.home import home
@@ -18,4 +23,15 @@ def create_app():
     app.register_blueprint(blog)
     app.register_blueprint(auth)
 
+    from .models import User
+
+    create_db(app)
+
     return app
+
+
+def create_db(app):
+    """Create the database if is does not exist."""
+    if not path.exists("instance/" + app.config["SQL_DB_NAME"]):
+        with app.app_context():
+            db.create_all()
