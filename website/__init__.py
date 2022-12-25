4e1cd75384f067296b475dbfced5b347b02a4a1f
Theodor Blom <me@theodorblom.com>
2022-12-25 22:02:00 +0100

    feat(database): multiple databases through binds

--- website/__init__.py
+++ website/__init__.py
@@ -4,11 +4,12 @@ from os import path
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_blogging import BloggingEngine, SQLAStorage
 
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app() -> Flask:
     """Create and return the flask-app."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
@@ -27,11 +28,13 @@ def create_app():
     from .models import User
 
     create_db(app)
+    blog_engine = init_blog(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
+    @blog_engine.user_loader
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
@@ -39,10 +42,29 @@ def create_app():
     return app
 
 
-def create_db(app):
+def create_db(app) -> None:
     """Create the database if is does not exist."""
-    if not path.exists("instance/" + app.config["SQL_DB_NAME"]):
-        app.logger.error("Database was not found!")
+    if not path.exists("instance/" + app.config["SQLALCHEMY_BINDS"]["auth"]):
+        app.logger.error(
+            f"Database {app.config['SQLALCHEMY_BINDS']['auth']} was not found!"
+        )
         with app.app_context():
-            db.create_all()
+            db.create_all(bind_key="auth")
             app.logger.info("Created new database.")
+    if not path.exists("instance/" + app.config["SQLALCHEMY_BINDS"]["blog"]):
+        app.logger.error(
+            f"Database {app.config['SQLALCHEMY_BINDS']['blog']} was not found!"
+        )
+        with app.app_context():
+            db.create_all(bind_key="blog")
+            app.logger.info("Created new database.")
+
+
+def init_blog(app) -> BloggingEngine:
+    """Initialize flask_blogging."""
+    with app.app_context():
+        blog_engine = BloggingEngine()
+        storage = SQLAStorage(engine=db.engine, bind="blog")
+        blog_engine.init_app(app, storage)
+
+    return blog_engine
--- website/models.py
+++ website/models.py
@@ -8,6 +8,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     """Database model for a website user."""
 
+    __bind_key__ = "auth"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
