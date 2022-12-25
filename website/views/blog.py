e0bab3475545040bdcf4f01b000378a8d1c0f002
Theodor Blom <me@theodorblom.com>
2022-12-25 13:51:40 +0100

    feat(blog): create-post page and navigation

--- website/views/blog.py
+++ website/views/blog.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """Views for the /blog url."""
 
-from flask import Blueprint, render_template
-from flask_login import current_user
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_required
 
 blog = Blueprint('blog', __name__, url_prefix='/blog')
 
@@ -11,3 +11,10 @@ blog = Blueprint('blog', __name__, url_prefix='/blog')
 def home():
     """Definition of the /blog site."""
     return render_template("blog/index.html", user=current_user)
+
+
+@blog.route('/create-post/')
+@login_required
+def create_post():
+    """Definition of the /blog/create-post site."""
+    return redirect(url_for('blogging.editor'))
