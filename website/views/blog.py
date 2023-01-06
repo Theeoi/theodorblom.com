382284206033ca9213271540e5ff227a3dee31ca
Theodor Blom <me@theodorblom.com>
2023-01-06 21:16:43 +0100

    feat(blog): flask_blogging enabled

--- website/views/blog.py
+++ website/views/blog.py
@@ -7,14 +7,16 @@ from flask_login import current_user, login_required
 blog = Blueprint('blog', __name__, url_prefix='/blog')
 
 
-@blog.route('/')
-def home():
-    """Definition of the /blog site."""
-    return render_template("blog/index.html", user=current_user)
-
-
-@blog.route('/create-post/')
-@login_required
-def create_post():
-    """Definition of the /blog/create-post site."""
-    return redirect(url_for('blogging.editor'))
+# @blog.route('/')
+# def home():
+#     """Definition of the /blog site."""
+#     return redirect(url_for("blogging.index"))
+#     # return render_template("blog/index.html", user=current_user)
+#
+#
+# @blog.route('/editor/')
+# @login_required
+# def create_post():
+#     """Definition of the /blog/editor site."""
+#     return redirect(url_for("blogging.editor"))
+#     # return render_template("blog/editor.html", user=current_user)
