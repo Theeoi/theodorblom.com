5c8c402fec96eee2b1068f5fc592d016bd6e1c99
Theodor Blom <me@theodorblom.com>
2023-01-07 18:21:37 +0100

    feat(blog): removed flask_blogging

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,6 @@ from os import path
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_blogging import BloggingEngine, SQLAStorage
 
 db = SQLAlchemy()
 
@@ -19,22 +18,20 @@ def create_app() -> Flask:
     # import and register views here
     from .views.home import home
     from .views.auth import auth
-    # from .views.blog import blog
+    from .views.blog import blog
 
     app.register_blueprint(home)
     app.register_blueprint(auth)
-    # app.register_blueprint(blog)
+    app.register_blueprint(blog)
 
     from .models import User
 
     create_db(app)
-    blog_engine = init_blog(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
-    @blog_engine.user_loader
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
@@ -44,27 +41,19 @@ def create_app() -> Flask:
 
 def create_db(app) -> None:
     """Create the database if is does not exist."""
-    if not path.exists("instance/" + app.config["SQLALCHEMY_BINDS"]["auth"]):
+    if not path.exists("instance/" +
+                       app.config["SQLALCHEMY_BINDS"]["auth"].split('/')[-1]):
         app.logger.error(
             f"Database {app.config['SQLALCHEMY_BINDS']['auth']} was not found!"
         )
         with app.app_context():
             db.create_all(bind_key="auth")
             app.logger.info("Created new database.")
-    if not path.exists("instance/" + app.config["SQLALCHEMY_BINDS"]["blog"]):
+    if not path.exists("instance/" +
+                       app.config["SQLALCHEMY_BINDS"]["blog"].split('/')[-1]):
         app.logger.error(
             f"Database {app.config['SQLALCHEMY_BINDS']['blog']} was not found!"
         )
         with app.app_context():
             db.create_all(bind_key="blog")
             app.logger.info("Created new database.")
-
-
-def init_blog(app) -> BloggingEngine:
-    """Initialize flask_blogging."""
-    with app.app_context():
-        blog_engine = BloggingEngine()
-        storage = SQLAStorage(engine=db.engine, bind="blog")
-        blog_engine.init_app(app, storage)
-
-    return blog_engine
--- website/views/blog.py
+++ website/views/blog.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 """
 Views for the /blog url.
-
-As the site is using the flask_blogging module these routes are not needed but
-the file is left for reference.
 """
 
-# from flask import Blueprint, render_template, redirect, url_for
-# from flask_login import current_user, login_required
-#
-# blog = Blueprint('blog', __name__, url_prefix='/blog')
-#
-#
-# @blog.route('/')
-# def home():
-#     """Definition of the /blog site."""
-#     return redirect(url_for("blogging.index"))
-#     return render_template("blog/index.html", user=current_user)
-#
-#
-# @blog.route('/editor/')
-# @login_required
-# def create_post():
-#     """Definition of the /blog/editor site."""
-#     return redirect(url_for("blogging.editor"))
-#     return render_template("blog/editor.html", user=current_user)
+from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+
+blog = Blueprint('blog', __name__, url_prefix='/blog')
+
+
+@blog.route('/')
+def home():
+    """Definition of the /blog site."""
+    return render_template("blog/index.html", user=current_user)
+
+
+@blog.route('/editor/')
+@login_required
+def create_post():
+    """Definition of the /blog/editor site."""
+    return render_template("blog/editor.html", user=current_user)
