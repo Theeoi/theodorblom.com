1df27c9b11f1439d9de22b04aea20d11e1df48dd
Theodor Blom <me@theodorblom.com>
2023-01-22 18:09:54 +0100

    feat(base): added animated favicon
    The website now enjoys an animated favicon similar to the 'type-it'
    package used in the navbar.
    
    Squashed commit of the following:
    
    commit 14da4dabdecb5008fbb29f6f730ae10334929236
    Author: Theodor Blom <me@theodorblom.com>
    Date:   Sun Jan 22 18:08:36 2023 +0100
    
        feat(base): added animated favicon

---
+++ website/static/js/base.js
@@ -0,0 +1,54 @@
+/* Nav Toggle */
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    });
+});
+
+/* Sticky Header */
+
+window.onscroll = function() {
+    stickyHeader();
+};
+
+var header = document.getElementById("sticky-header");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+// Animated favicon
+
+var favicon_images = [
+    "/static/resources/icons/theodorblom-favicon-nocursor.svg",
+    "/static/resources/icons/theodorblom-favicon-cursor.svg",
+];
+var counter = 0;
+
+setInterval(function() {
+    if (document.querySelector("link[rel='icon']") !== null)
+        document.querySelector("link[rel='icon']").remove();
+
+    document
+        .querySelector("head")
+        .insertAdjacentHTML(
+            "beforeend",
+            "<link rel='icon' href=" + favicon_images[counter] + "/>"
+        );
+
+    if (counter == favicon_images.length - 1) counter = 0;
+    else counter++;
+}, 500);
