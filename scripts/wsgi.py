2b46c219a1129fef627c26debf30b621a954d8b8
TheoB <71510665+Theeoi@users.noreply.github.com>
2024-02-01 09:05:27 +0100

    Adding mod_wsgi deployment scripts (#36)

---
+++ scripts/wsgi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from app import create_app
+
+
+def configure_logging():
+    logs_dir = Path.cwd() / "logs"
+
+    if not logs_dir.exists():
+        logs_dir.mkdir(parents=True)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=f"{logs_dir.absolute()}/default.log",
+        format="%(asctime)s %(message)s",
+    )
+
+
+configure_logging()
+
+application = create_app()
