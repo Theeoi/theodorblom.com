aa7d4a479aee38b6363d1d93831f1fd558fcb34a
Theodor Blom <me@theodorblom.com>
2024-02-06 12:08:44 +0100

    Updating tests to new layout

--- tests/unit/test_statistics.py
+++ tests/unit/test_statistics.py
@@ -5,7 +5,7 @@ from app.database.models import Request
 
 def test_no_static_request(test_client):
     test_client.get("/")
-    response = test_client.get("/static/resources/icons/theodorblom-favicon-cursor.svg")
+    response = test_client.get("/static/resources/icons/icon.png")
     assert response.status_code == 200
 
     request = Request.query.order_by(Request.date.desc()).first()
